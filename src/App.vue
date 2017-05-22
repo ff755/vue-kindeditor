0891355542c9fb7a5f93b83dbca9795bf586163c
Yoker.Wu <Yoker.Wu@gmail.com>
2017-05-22 19:07:36 +0800

    支持页面初始化后绑定变量赋值 & 支持双向绑定变量（vue2.3.0+）

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,11 @@
 <template>
   <div id="app">
-    <editor id="editor_id" height="500px" width="700px" :content="editorText"
+    <editor id="editor_id" height="500px" width="700px" :content.sync="editorText"
             :afterChange="afterChange()"
             pluginsPath="/static/kindeditor/plugins/"
             :loadStyleMode="false"
             @on-content-change="onContentChange"></editor>
+    <div> editorTextCopy: {{ editorTextCopy }} </div>
   </div>
 </template>
 
@@ -14,14 +15,18 @@ export default {
   name: 'app',
   data () {
     return {
-      editorText: ''
+      editorText: '直接初始化值', // 双向同步的变量
+      editorTextCopy: ''  // content-change 事件回掉改变的对象
     }
+  },
+  mounted () {
+    this.editorText = '<h3>页面载入绑定后，初始化值</h3>'
   },
   created () {
   },
   methods: {
     onContentChange (val) {
-      this.editorText = val
+      this.editorTextCopy = val
     },
     afterChange () {
     }
